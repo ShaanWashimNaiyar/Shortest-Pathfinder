10a4934955d33fd51160f631dd1fb52072e3825c
ShaanWashimNaiyar <shaanwashimnaiyar@gmail.com>
2025-04-17 11:59:39 +0530

    modified:   PathFinder/src/App.js

--- PathFinder/src/App.js
+++ PathFinder/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import PathfindingVisualizer from './PathfindingVisualizer/PathfindingVisualizer';
-
+ const a =0;
 function App() {
   return (
     <div className="App">
